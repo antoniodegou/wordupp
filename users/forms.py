2f660a4a4cbad4edc7742c52fa993b593616f3c3
antonio de gou <thestockofantonio@gmail.com>
2023-09-23 10:23:01 +0100

    made sure user has unique email when registering

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserChangeForm
+from django.core.exceptions import ValidationError
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,6 +10,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        print("clean_email is being called!")  # Debugging print statement
+
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already exists")
+        return email
 
 
 class UserUpdateForm(UserChangeForm):
